af9e8c0d999a15698346a0568fcd853b4b859cd7
Kamil Noga <nogakamil@gmail.com>
2023-08-14 01:00:14 +0200

    mongo user

---
+++ scripts/mongo-init.js
@@ -0,0 +1,17 @@
+const user = _getEnv("MONGO_INITDB_ROOT_USERNAME");
+const pwd = _getEnv("MONGO_INITDB_ROOT_PASSWORD");
+
+db.auth(user, pwd);
+
+db = db.getSiblingDB("db");
+
+db.createUser({
+  user: _getEnv("MONGO_INITDB_ROOT_USERNAME"),
+  pwd: _getEnv("MONGO_INITDB_ROOT_PASSWORD"),
+  roles: [
+    {
+      role: "readWrite",
+      db: "db",
+    },
+  ],
+});
